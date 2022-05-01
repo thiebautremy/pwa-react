8f372368873dd47f8eddbfc169e1fb58abfd29ee
Rémy Thiebaut <remy.thiebaut@actimage.com>
2022-05-01 17:45:58 +0200

    change readMe, bg color and cache path

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "version-1";
-const urlsToCache = ["index.html", "offline.html", "../src/assets/videos"];
+const urlsToCache = ["index.html", "offline.html"];
 
 const self = this;
 
